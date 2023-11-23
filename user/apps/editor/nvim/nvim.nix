e7a0ca8d55804de5a4c1775605a001f235bf7ea7
Abel Chartier <abel.chartier@epita.fr>
2023-11-23 10:25:53 +0100

    nvim

--- user/apps/editor/nvim/nvim.nix
+++ user/apps/editor/nvim/nvim.nix
@@ -1,33 +1,5 @@
 { pkgs, ... }:
-let
 
-  treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-    p.bash
-    p.comment
-    p.css
-    p.dockerfile
-    p.fish
-    p.gitattributes
-    p.gitignore
-    p.go
-    p.gomod
-    p.gowork
-    p.hcl
-    p.javascript
-    p.jq
-    p.json5
-    p.json
-    p.make
-    p.markdown
-    p.nix
-    p.python
-    p.rust
-    p.toml
-    p.typescript
-    p.vue
-    p.yaml
-  ]));
-in
 {
   home.packages = with pkgs; [
     ripgrep
@@ -35,26 +7,43 @@ in
     lua-language-server
   ];
 
-  programs.neovim = {
+  programs.neovim = 
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
     enable = true;
     vimAlias = true;
     coc.enable = false;
 
-    plugins = [
-      treesitterWithGrammars
+    plugins = with pkgs.vimPlugins; [
+        
+      nvim-lspconfig
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-luasnip
+      friendly-snippets
+
+      {
+        plugin = lsp-zero;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+
+
     ];
   };
 
-  home.file."./.config/nvim/" = {
+/*   home.file."./.config/nvim/" = {
     source = ./nvim;
     recursive = true;
-  };
+  }; */
 
   # Treesitter is configured as a locally developed module in lazy.nvim
   # we hardcode a symlink here so that we can refer to it in our lazy config
-  home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
+/*   home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
     recursive = true;
     source = treesitterWithGrammars;
-  };
+  }; */
 
 }
