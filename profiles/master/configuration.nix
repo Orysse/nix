c74d0b3a56b14a58161f0c3871bd6905758aeb1e
Abel Chartier <abel.chartier@epita.fr>
2023-11-23 09:40:32 +0100

    fix master4

--- profiles/master/configuration.nix
+++ profiles/master/configuration.nix
@@ -47,18 +47,6 @@
 
   i18n.defaultLocale = locale;
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = locale;
-    LC_IDENTIFICATION = locale;
-    LC_MEASUREMENT = locale;
-    LC_MONETARY = locale;
-    LC_NAME = locale;
-    LC_NUMERIC = locale;
-    LC_PAPER = locale;
-    LC_TELEPHONE = locale;
-    LC_TIME = locale;
-  };
-
   services.xserver = {
     layout = "us";
     xkbVariant = "";
