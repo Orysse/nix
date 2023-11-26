3ea0b29baa7849055a7f725131a443336f1488cd
Abel Chartier <abel.chartier@epita.fr>
2023-11-26 15:32:16 +0100

    everforest3?

--- user/apps/utils/gtk.nix
+++ user/apps/utils/gtk.nix
@@ -4,8 +4,8 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Everforest-GTK-Theme";
-      package = pkgs.callPackage ../../../packages/everforest-gtk.nix;/* .override {
+      name = "Everforest-Dark-BL";
+      package = pkgs.callPackage ./everforest-gtk-theme.nix { };/* .override {
         accents = [ "mauve" ];
         size = "standard";
         tweaks = [ ];
