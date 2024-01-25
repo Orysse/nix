96e007f478f1d9b3d51fd3d2bc3791b85c9d7674
Abel <abel.chartier@gmail.com>
2024-01-25 19:10:31 +0100

    better 20min

--- user/apps/utils/betterlockscreen.nix
+++ user/apps/utils/betterlockscreen.nix
@@ -1,5 +1,6 @@
 {
     services.betterlockscreen.enable = true;
+    services.betterlockscreen.inactiveInterval = 25;
     services.betterlockscreen.arguments = [
         "-u .dotfiles/user/wallpaper/wallpaper.jpg"
     ];
