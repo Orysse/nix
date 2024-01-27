04844875b9b44380ba5204fc46f4d5792f83d239
Abel <abel.chartier@gmail.com>
2024-01-27 22:17:58 +0100

    new fonts

--- profiles/master/configuration.nix
+++ profiles/master/configuration.nix
@@ -191,7 +191,7 @@
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "IosevkaTerm" "Iosevka" ]; })
     rounded-mgenplus
-    estrangelo
+    noto-sans-imperial-aramaic
   ];
 
   # Enable the OpenSSH daemon.
