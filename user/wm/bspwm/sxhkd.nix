863ca007d4322e06528aaf8c93abe0e3a551551c
Abel <abel.chartier@gmail.com>
2024-02-01 18:53:34 +0100

    bspc

--- user/wm/bspwm/sxhkd.nix
+++ user/wm/bspwm/sxhkd.nix
@@ -4,10 +4,10 @@
     keybindings = {
       "super + {Return, d, p, r, m}" = "{alacritty, rofi -show drun, ~/.config/rofi/leave.sh, rofi -show run, ./.config/rofi/music.sh}";
       "super + {_,shift + }q" = "bspc node -{c,k}";
-      "super + {_,shift + }{1-9,0}" = "bspc {desktop -f,node -d} '^{1-9,10}'";
+      "super + {_,shift + }{1-8}" = "bspc {desktop -f,node -d} '^{1-8}'";
       "super + {_,shift + }{h, j, k, l}" = "bspc node -{f,s} {west,south,north,east}";
       "super + x" = "betterlockscreen -l";
-      "alt + Tab" = "rofi -show window";
+      "alt + {_,shift + } Tab" = "{bspc -desktop -f next.occupied, rofi -show window}";
       "alt + {j, k}" = "{amixer -q sset Master 5%-, amixer -q sset Master 5%+}";
       "super + f" = "bspc node -t \"~\"{floating,tiled}";
     };
